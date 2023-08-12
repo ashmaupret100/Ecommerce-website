918475be820421910aaaf5460aa3ddee399372f5
Ashma Upreti <ashma.upreti99@gmail.com>
2023-08-11 23:03:17 -0400

    feat:registration page

--- frontend/src/app/register/page.tsx
+++ frontend/src/app/register/page.tsx
@@ -1,8 +1,44 @@
 "use client";
 import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 
 function Register() {
-  return <div>Register</div>;
+  const initialValues = {
+    name: "",
+    username: "",
+    email: "",
+    password: "",
+    confirmpassword: "",
+  };
+
+  const validationSchema = Yup.object({
+    name: Yup.string().required("Please enter your Name."),
+    username: Yup.string().required("Please enter your username."),
+    email: Yup.string().required("Please enter valid email address."),
+    password: Yup.string().required("Please enter correct password"),
+    confirmpassword: Yup.string()
+      .required("Please confirm password")
+      .oneOf([Yup.ref("password")], "Password must match"),
+  });
+
+  const onSubmit = () => {
+    console.log("nothing");
+  };
+  return (
+    <div>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={onSubmit}>
+        <Form>
+          <Field type="text" name="username" />
+          <Field type="email" name="email" />
+          <Field type="password" name="password" />
+        </Form>
+      </Formik>
+    </div>
+  );
 }
 
 export default Register;
