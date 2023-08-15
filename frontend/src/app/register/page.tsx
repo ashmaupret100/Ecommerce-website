bb71a402cc61015cb73080e7884371548b2be7fe
Ashma Upreti <ashma.upreti99@gmail.com>
2023-08-14 23:05:47 -0400

    feat: add register form and functionality

--- frontend/src/app/register/page.tsx
+++ frontend/src/app/register/page.tsx
@@ -1,9 +1,13 @@
 "use client";
 import React from "react";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 
 function Register() {
+  const router = useRouter();
   const initialValues = {
     name: "",
     username: "",
@@ -21,20 +25,45 @@ function Register() {
       .required("Please confirm password")
       .oneOf([Yup.ref("password")], "Password must match"),
   });
+  function handleSubmit(values: typeof initialValues) {
+    axios
+      .post("http://localhost:4000/users", {
+        email: values.email,
+        password: values.password,
+      })
+      .then((response) => {
+        console.log(response);
+        localStorage.setItem("token", response.data.accessToken);
+        console.log(response.data.accessToken);
+        toast.success("Scuccessfully created your account.");
+        router.push("/login");
+      })
+      .catch((e) => {
+        toast.error("Unable to Sign In");
+      });
+  }
 
-  const onSubmit = () => {
-    console.log("nothing");
-  };
   return (
     <div>
       <Formik
         initialValues={initialValues}
         validationSchema={validationSchema}
-        onSubmit={onSubmit}>
+        onSubmit={handleSubmit}>
         <Form>
+          <label>Name</label>
+          <Field type="text" name="name" />
+
+          <label>User Name</label>
           <Field type="text" name="username" />
+          <label>Email</label>
           <Field type="email" name="email" />
+          <label>Password</label>
           <Field type="password" name="password" />
+          <label>Re Enter Password</label>
+          <Field type="password" name="confirmpassword" />
+          <button type="submit" className="bg-red-100">
+            Register
+          </button>
         </Form>
       </Formik>
     </div>
